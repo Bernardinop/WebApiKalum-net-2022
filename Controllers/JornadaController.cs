bc43b768686923b7b2c070a89a739efd5dfa41d5
bernardino Payeras <josemol316p@gmail.com>
2022-06-20 21:47:16 -0600

    Creando procesos para Jornada

--- Controllers/JornadaController.cs
+++ Controllers/JornadaController.cs
@@ -5,15 +5,14 @@ using WebApiKalum.Entities;
 namespace WebApiKalum.Controllers
 {
     [ApiController]
-    [Route("v1/KalumManagement/[controller]")]
+    [Route("v1/KalumManagement/Jornada")]
     public class JornadaController : ControllerBase
     {
         private readonly KalumDbContext DbContext;
         private readonly ILogger<JornadaController> Logger;
-
-        public JornadaController(KalumDbContext dbContext, ILogger<JornadaController> _Logger)
+        public JornadaController(KalumDbContext _DbContext, ILogger<JornadaController> _Logger)
         {
-            this.DbContext = dbContext;
+            this.DbContext = _DbContext;
             this.Logger = _Logger;
         }
 
@@ -23,8 +22,8 @@ namespace WebApiKalum.Controllers
             List<Jornada> jornadas = null;
             Logger.LogDebug("Iniciando proceso consulta jornadas");
             jornadas = await DbContext.Jornada.Include(j => j.Aspirantes).Include(j => j.Inscripciones).ToListAsync();
-            
-            if(jornadas == null || jornadas.Count==0)
+
+            if (jornadas == null || jornadas.Count == 0)
             {
                 Logger.LogWarning("No existen jornadas");
                 return new NoContentResult();
@@ -38,13 +37,60 @@ namespace WebApiKalum.Controllers
         {
             Logger.LogDebug("Iniciando proceso de busqueda con el id " + id);
             var jornada = await DbContext.Jornada.Include(j => j.Aspirantes).Include(j => j.Inscripciones).FirstOrDefaultAsync(j => j.JornadaId == id);
-            if(jornada == null)
+            if (jornada == null)
             {
                 Logger.LogWarning("No existe la jornada con el id " + id);
                 return new NoContentResult();
             }
             Logger.LogInformation("Finalizando el proceso de busqueda de forma exitosa");
             return Ok(jornada);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Jornada>> Post([FromBody] Jornada value)
+        {
+            Logger.LogDebug("Iniciando el proceso de agregar una nueva jornada");
+            value.JornadaId = Guid.NewGuid().ToString().ToUpper();
+            await DbContext.Jornada.AddAsync(value);
+            await DbContext.SaveChangesAsync();
+            Logger.LogInformation("Finalizando el proceso para agregar una nueva jornada");
+            return new CreatedAtRouteResult("GetJornada", new { id = value.JornadaId }, value);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Jornada>> Delete(string id)
+        {
+            Jornada jornada = await DbContext.Jornada.FirstOrDefaultAsync(j => j.JornadaId == id);
+            if (jornada == null)
+            {
+                Logger.LogWarning($"No se encontro ninguna Jornada con el ID {id}");
+                return NotFound();
+            }
+            else
+            {
+                DbContext.Jornada.Remove(jornada);
+                await DbContext.SaveChangesAsync();
+                Logger.LogInformation($"Se ha eliminado correctamente la jornada con el ID {id}");
+                return jornada;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(string id, [FromBody] Jornada value)
+        {
+            Logger.LogDebug($"Iniciando el proceso de actualizacion de la Jornada con el ID {id}");
+            Jornada jornada = await DbContext.Jornada.FirstOrDefaultAsync(j => j.JornadaId == id);
+            if (jornada == null)
+            {
+                Logger.LogWarning($"No existe la Jornada con el ID {id}");
+                return BadRequest();
+            }
+            jornada.NombreCorto = value.NombreCorto;
+            jornada.Descripcion = value.Descripcion;
+            DbContext.Entry(jornada).State = EntityState.Modified;
+            await DbContext.SaveChangesAsync();
+            Logger.LogInformation($"Los datos de la jornada con el ID {id} han sido actualizados correctamente");
+            return NoContent();
         }
     }
 }
